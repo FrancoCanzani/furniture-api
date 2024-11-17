6fc7fe21ab44d5919863f58c54902838a0c35ffb
Franco Canzani <francocanzani@gmail.com>
2024-11-17 20:49:40 +0100

    increase rate limit

--- src/lib/rate-limit.ts
+++ src/lib/rate-limit.ts
@@ -2,11 +2,11 @@ import { rateLimit } from 'express-rate-limit';
 
 export const dailyLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hours
-  limit: 200, // Limit each IP to 100 requests per day
+  limit: 500, // Limit each IP to 100 requests per day
   standardHeaders: 'draft-7', // Returns RateLimit header
   legacyHeaders: false,
   message: {
     error: 'Too many requests from this IP, please try again in 24 hours',
-    details: 'Rate limit: 200 requests per day',
+    details: 'Rate limit: 500 requests per day',
   },
 });
